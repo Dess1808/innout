fa0acee335fcc79317b4f7a6c425325033ae9b7c
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2025-08-28 21:57:43 -0400

    criado a tela de login, ainda sem validacao

--- public/index.php
+++ public/index.php
@@ -3,11 +3,5 @@ ini_set('display_errors', 1);
 
 //exports
 require_once(realpath(dirname(__FILE__, 2) . '/src/config/config.php'));
-require_once(realpath(dirname(__FILE__, 2) . '/src/models/User.php'));
-
-print_r(User::getResultFromDataBase([
-    'name' => 'Chaves'
-], 
-    'name, email, is_admin'));
-
+require_once(realpath(dirname(__FILE__, 2) . '/src/views/login.php'));
 
