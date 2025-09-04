18580f781bf64af747057b1810a33d4bd674b931
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2025-09-04 15:26:53 -0400

    add redirecionamento na pagina index para login

--- public/index.php
+++ public/index.php
@@ -3,5 +3,14 @@ ini_set('display_errors', 1);
 
 //exports
 require_once(realpath(dirname(__FILE__, 2) . '/src/config/config.php'));
-require_once(realpath(CONTROLLER_PATH . '/login.php'));
+
+$uri = urldecode($_SERVER['REQUEST_URI']);
+
+if ($uri === '/' || $uri === ' ' || $uri === '/index.php'){
+    $uri = '/login.php';
+}
+
+//load view 
+require_once(CONTROLLER_PATH . "{$uri}");
+
 
--- src/config/loader.php
+++ src/config/loader.php
@@ -21,3 +21,18 @@ function loadView($viewName, $params = array()){
     //carregando view login com variaveis tratadas !
     require_once(VIEW_PATH . "/{$viewName}.php");
 }
+
+//loader View encapsuladas, carregar varias views para montar um pagina completa
+function loadTemplateView($viewName, $params = array()){
+
+    if (count($params) > 0){
+        foreach($params as $key => $value){
+            if (strlen($key) > 0){
+                //variavel variavel
+                ${$key} = $value;
+            }
+        }
+    }
+    
+    require_once(VIEW_PATH . "/{$viewName}.php");
+}
