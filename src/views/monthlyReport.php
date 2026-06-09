68e43d6dcb36a37482506eb96066900fad1713ac
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2026-06-09 14:40:58 -0400

    implementado em tela filtro com mes e ano

--- src/views/monthlyReport.php
+++ src/views/monthlyReport.php
@@ -7,27 +7,45 @@
         );
     ?>
 
-    <table>
-        <thead>
-            <th>Dia</th>
-            <th>Entrada 1</th>
-            <th>Saída 1</th>
-            <th>Entrada 2</th>
-            <th>Saída 2</th>
-            <th>Saldo</th>
-        </thead>
+    <div>
+        <form action="#" class="mb-4" method="POST">
+            <select name="period" class="form-control" placeholder="select the period...">
+                <?php
+                    foreach($period as $key => $value){
+                        echo "<option value='{$key}'>$value</option>";
+                    }
+                ?>
+            </select>
+        </form>
 
-        <tbody>
-            <?php foreach($report as $registry):?>
-                <tr>
-                    <td><?=$registry->work_date?></td>
-                    <td><?=$registry->time1?></td>
-                    <td><?=$registry->time2?></td>
-                    <td><?=$registry->time3?></td>
-                    <td><?=$registry->time4?></td>
-                    <td><?="saldo"?></td> 
+        <table class="table table-bordered table-striped table-hover">
+            <thead>
+                <th>Dia</th>
+                <th>Entrada 1</th>
+                <th>Saída 1</th>
+                <th>Entrada 2</th>
+                <th>Saída 2</th>
+                <th>Saldo</th>
+            </thead>
+
+            <tbody>
+                <?php foreach($report as $registry):?>
+                    <tr>
+                        <td><?=dateFormettedWithLocal($registry->work_date, "eee, d 'de' MMM 'de' YYYY")?></td>
+                        <td><?=$registry->time1?></td>
+                        <td><?=$registry->time2?></td>
+                        <td><?=$registry->time3?></td>
+                        <td><?=$registry->time4?></td>
+                        <td><?=$registry->getBalance()?></td> 
+                    </tr>
+                <?php endforeach;?>
+                <tr class="bg-primary text-white">
+                    <td>Horas Trabalhadas</td>
+                    <td colspan="3"><?=$sumWorkedOfTime?></td>
+                    <td>Saldo Mensal</td>
+                    <td><?=$balance?></td>
                 </tr>
-            <?php endforeach;?>
-        </tbody>
-    </table>
+            </tbody>
+        </table>
+    </div>
 </main>
