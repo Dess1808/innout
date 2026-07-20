1f5a228b70861c87bb314811001c001eac5b9ae9
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2026-07-20 14:02:30 -0400

    adicionado os primeiros estilos managerReport, continuar...

--- src/views/managerReport.php
+++ src/views/managerReport.php
@@ -1,8 +1,34 @@
 <main class="content">
     <?php
-        echo "Quantidade de Usuários ativos: {$activeUsers}<br>";
-        echo "Quantidade de horas trabalhadas do mês: {$hoursInMonth}<br>";
-        print_r($absentUsers);
+        renderTitle(
+            'Relatório Gerencial',
+            'Resumo da horas trabalhadas dos funcionários',
+            'icofont-chart-histogram'
+        )
     ?>
+
+    <div class="summary-boxes">
+        <!-- ACTIVE USERS -->
+        <div class="summary-box bg-primary">
+            <i class="icofont-users"></i>
+            <p class="title">Qtd de funcionários</p>
+            <h3 class="value"><?=$activeUsers?></h3>
+        </div>
+        <!-- ABSENT USERS -->
+        <div class="summary-box bg-danger">
+            <i class="icofont-patient-bed"></i>
+            <p class="title">Faltas</p>
+            <h3 class="value"><?=count($absentUsers)?></h3>
+        </div>
+        <!-- HOURS IN MONTH -->
+         <div class="summary-box bg-success">
+            <i class="icofont-sand-clock"></i>
+            <p class="title">Horas no mês</p>
+            <h3 class="value"><?=$hoursInMonth?></h3>
+        </div>
+    </div>
+
+    <!-- Card absent users -->
+     
 </main>
 
