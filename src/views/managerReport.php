16b3f6a61107fd538c6d03e321d9ebe6032754ee
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2026-07-22 12:11:14 -0400

    finalizado estilos managerReport

--- src/views/managerReport.php
+++ src/views/managerReport.php
@@ -10,19 +10,19 @@
     <div class="summary-boxes">
         <!-- ACTIVE USERS -->
         <div class="summary-box bg-primary">
-            <i class="icofont-users"></i>
+            <i class="icon icofont-users"></i>
             <p class="title">Qtd de funcionários</p>
             <h3 class="value"><?=$activeUsers?></h3>
         </div>
         <!-- ABSENT USERS -->
         <div class="summary-box bg-danger">
-            <i class="icofont-patient-bed"></i>
+            <i class="icon icofont-patient-bed"></i>
             <p class="title">Faltas</p>
             <h3 class="value"><?=count($absentUsers)?></h3>
         </div>
         <!-- HOURS IN MONTH -->
          <div class="summary-box bg-success">
-            <i class="icofont-sand-clock"></i>
+            <i class="icon icofont-sand-clock"></i>
             <p class="title">Horas no mês</p>
             <h3 class="value"><?=$hoursInMonth?></h3>
         </div>
