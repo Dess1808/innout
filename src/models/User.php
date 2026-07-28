05d93039f8d5d3ae1e951cea3b259225b436a4d8
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2026-07-28 14:35:41 -0400

    criando as validações do formulario de cadastro de usuario..

--- src/models/User.php
+++ src/models/User.php
@@ -26,10 +26,34 @@ class User extends Model {
     */
     public function insertFromDataGenerator() {
         //converting to 'number'
+        $this->validateRegister();
         $this->is_admin = $this->is_admin ? 1 : 0;
         if (!$this->end_date) $this->end_date = null;
+
+        //criptrography password
+        $this->password = password_hash($this->password, PASSWORD_DEFAULT);
 
         return parent::insertFromDataGenerator();
     }
 
+    //validations form
+    private function validateRegister(){
+        $errors = [];
+
+        //name
+        if (!$this->name) {
+            $errors['name'] = 'Nome é obrigatório';
+        }
+        //email
+        if (!$this->email) {
+            $errors['email'] = 'Email é obrigatório';
+        } else {
+            
+        }
+        //password
+        //confirm_password
+        //start_date
+        //end_date
+
+    }
 }
