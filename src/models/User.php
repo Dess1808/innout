6579021b57803eef72ef5ff5c692e5c493c48763
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2026-07-29 14:56:43 -0400

    finalizado cadastro de usuario com as validações de campos

--- src/models/User.php
+++ src/models/User.php
@@ -33,6 +33,7 @@ class User extends Model {
         //criptrography password
         $this->password = password_hash($this->password, PASSWORD_DEFAULT);
 
+        //chamando insertFromDataGenerator() pai para inseriro no banco
         return parent::insertFromDataGenerator();
     }
 
@@ -48,12 +49,46 @@ class User extends Model {
         if (!$this->email) {
             $errors['email'] = 'Email é obrigatório';
         } else {
-            
+            if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)){
+                $errors['email'] = 'Email é inválido';
+            }
         }
+        
         //password
-        //confirm_password
-        //start_date
-        //end_date
+        if (!$this->password){
+            $errors['password'] = 'Senha é obrigatória';
+        }
 
+        //confirm_password
+        if (!$this->confirm_password){
+            $errors['confirm_password'] = 'Confirmar senha é obrigatório';
+        }
+
+        //password equal confirm_password   
+        if (($this->password && $this->confirm_password) && 
+                ($this->password != $this->confirm_password)){
+                    $errors['password'] = 'Senhas devem ser iguais';
+                    $errors['confirm_password'] = 'Senhas devem ser iguais';
+        }
+
+        //start_date
+        if (!$this->start_date){
+            $errors['start_date'] = 'Data de admissão é obrigatória';
+        } elseif (!DateTime::createFromFormat('Y-d-m', $this->start_date)){
+            $errors['start_date'] = 'Data de admissão é inválida, formato correto dd/mm/YYYY';
+        }
+
+        // var_dump($this->start_date);
+        // die();
+
+        //end_date
+        if ($this->end_date && !DateTime::createFromFormat('Y-m-d', $this->end_date)){
+            $errors['end_date'] = 'Data de desligamento é inválida, formato correto dd/mm/YYYY';
+        }
+
+        //throw exception!
+        if (count($errors) > 0){
+            throw new ValidationException($errors);
+        }
     }
 }
